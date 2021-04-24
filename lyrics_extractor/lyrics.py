e191cdc5fda4dc007fd22d76431c9a5d5ca187e1
mkryska11 <kryska.marek@gmail.com>
2021-04-24 16:40:38 +0200

    feat: genius added, dashes removed from query

--- lyrics_extractor/lyrics.py
+++ lyrics_extractor/lyrics.py
@@ -5,6 +5,11 @@ from ScrapeSearchEngine.SearchEngine import Google
 from urllib.parse import urlparse
 
 _user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36'
+_sites = [
+    "genius.com",
+    "tekstowo.pl"
+    ]
+_sites_query = ' OR '.join(map(lambda s: f'site:{s}', _sites))
 
 class LyricScraperException(Exception):
     """Handles all lyrics extractor exceptions."""
@@ -21,6 +26,34 @@ class _ScraperFactory:
 
     def _update_title(self, title):
         self.title = title
+
+    def _genius_scraper_method_1(self):
+        extract = self.source_code.select(".lyrics")
+        if not extract:
+            return None
+
+        lyrics = (extract[0].get_text()).replace('<br>', '\n').strip()
+        return lyrics
+
+    def _genius_scraper_method_2(self):
+        all_extracts = self.source_code.select(
+            'div[class*="Lyrics__Container-sc-"]')
+        if not all_extracts:
+            return None
+
+        lyrics = ''
+        for extract in all_extracts:
+            for br in extract.find_all("br"):
+                br.replace_with("\n")
+            lyrics += extract.get_text()
+
+        return lyrics.strip()
+
+    def genius_scraper(self):
+        lyrics = self._genius_scraper_method_1() or self._genius_scraper_method_2()
+        self._update_title(self.title[:-16])
+
+        return lyrics
 
     def tekstowo_scraper(self):
         extract = self.source_code.select(".song-text")
@@ -39,11 +72,13 @@ class SongLyrics:
 
     scraper_factory = _ScraperFactory()
     scrapers = {
+        'genius': scraper_factory.genius_scraper,
         'tekstowo': scraper_factory.tekstowo_scraper,
     }
 
-    def __handle_search_request(self, song_name):
-        query = f'{song_name} site:tekstowo.pl'
+    def __handle_search_request(self, song_name: str):
+        query = f'{song_name.replace("-", " ")} {_sites_query}'
+        print(query)
         titles, links = Google(query, _user_agent)
         if len(links) == 1 and urlparse(links[0]).path:
             raise LyricScraperException(status_code)
